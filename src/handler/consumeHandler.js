cb0b756959a11fb27d120f140bf8d13171166c80
Sylvain <sylvain.denyse@gmail.com>
2018-05-17 18:33:43 +0200

    clean API datas

--- src/handler/consumeHandler.js
+++ src/handler/consumeHandler.js
@@ -5,14 +5,23 @@ export const consumeHandler = {
         const reply = get(client, 'mousemove')
 
         return reply.then((res) => {
-            return res
+            return cleanData(res)
         })
     },
     getClick: (request, h) => {
         const reply = get(client, 'click')
 
         return reply.then((res) => {
-            return res
+            return cleanData(res)
         })
     }
+}
+
+const cleanData = data => {
+    let cleaned = []
+    for (let i in data) {
+        cleaned.push(data[i])
+    }
+
+    return cleaned
 }
